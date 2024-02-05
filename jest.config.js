7872a8cfd25478cc2686b6e99eb8e245e4d8127d
online-marathon <61456363+online-marathon@users.noreply.github.com>
2024-02-05 10:57:39 +0200

    Initialize develop

---
+++ jest.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  roots: ['<rootDir>/test'],
+  moduleNameMapper: {
+    '^~/(.*)$': '<rootDir>/$1'
+  },
+  verbose: true,
+  testEnvironment: 'node',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '!<rootDir>/node_modules/*',
+    '!<rootDir>/test/**/*',
+    '!<rootDir>/consts/*',
+    '!<rootDir>/configs/*',
+    '!<rootDir>/docs/*',
+    '!<rootDir>/emails/*',
+    '!<rootDir>/*.json',
+    '!<rootDir>/*.yaml'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  },
+  coverageReporters: ['html', 'lcov'],
+  coverageDirectory: '<rootDir>/test/coverage',
+  testTimeout: 12000,
+  testMatch: ['<rootDir>/test/integration/**/*.spec.js', '<rootDir>/test/unit/**/*.spec.js'],
+  testResultsProcessor: 'jest-sonar-reporter'
+}
