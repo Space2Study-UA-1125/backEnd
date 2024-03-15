5288a0a3600a9317e0ee6619f83de460e72ff547
Stas <cnfchecyfr@gmail.com>
2024-03-15 12:20:34 +0200

    updated userService for password

--- controllers/user.js
+++ controllers/user.js
@@ -2,7 +2,7 @@ const userService = require('~/services/user')
 const { createForbiddenError, createError } = require('~/utils/errorsHelper')
 const { hashPassword, comparePasswords } = require('~/utils/passwordHelper')
 const createAggregateOptions = require('~/utils/users/createAggregateOptions')
-const { INCORRECT_CREDENTIALS } = require('~/consts/errors')
+const { INCORRECT_CREDENTIALS, WRONG_CURRENT_PASSWORD } = require('~/consts/errors')
 const getUsers = async (req, res) => {
   const { skip, limit, sort, match } = createAggregateOptions(req.query)
 
@@ -30,7 +30,7 @@ const updateUser = async (req, res) => {
   if (updateData.password) {
     const userById = await userService.getUserById(id)
     const user = await userService.getUserByEmail(userById.email)
-
+    if (!(await comparePasswords(updateData.oldPassword, user.password))) throw createError(401, WRONG_CURRENT_PASSWORD)
     if (await comparePasswords(updateData.password, user.password)) throw createError(401, INCORRECT_CREDENTIALS)
 
     const hashedPassword = await hashPassword(updateData.password)
